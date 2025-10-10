4ee9bc4e161100c3a848c853f51d5275e40dbf5b
Dung160405 <duongtridung1604@gmail.com>
2025-10-10 16:17:57 +0700

    add:Lọc bệnh nhân theo giới tính

--- OOP/oop_ui/src/main/java/org/example/oop/Control/PatientAndPrescription/PatientListController.java
+++ OOP/oop_ui/src/main/java/org/example/oop/Control/PatientAndPrescription/PatientListController.java
@@ -8,6 +8,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.Region;
 import org.example.oop.Model.PatientAndPrescription.PatientRecord;
 
 import java.net.URL;
@@ -35,15 +36,17 @@ public class PatientListController implements Initializable {
     @FXML
     private ChoiceBox<String> genderFilter;
     @FXML
-    private ChoiceBox<String> genderFilter1;
+    private ChoiceBox<String> dobFromFilter;
     @FXML
-    private ChoiceBox<String> genderFilter2;
+    private ChoiceBox<String> dobToFilter;
     @FXML
-    private Button newPatientButton;
+    private Button applyFilterButton;
 
     ObservableList<PatientRecord> data;
+    ObservableList<PatientRecord> dataView;
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+
         // Cấu hình các cột
         id.setCellValueFactory(c -> new ReadOnlyObjectWrapper<>(c.getValue().getId()));
         nameColumn.setCellValueFactory(c -> new ReadOnlyObjectWrapper<>(c.getValue().getNamePatient()));
@@ -74,31 +77,85 @@ public class PatientListController implements Initializable {
                 new PatientRecord(2, "Jane Smith", 102, LocalDate.of(1992, 5, 15),
                         PatientRecord.Gender.NỮ, "456 Avenue", "0987654321", "jane@email.com")
         );
-        patientTable.setItems(data);
+        dataView = FXCollections.observableArrayList(data);
+
+        patientTable.setItems(dataView);
 
         // Cấu hình filter giới tính
         genderFilter.setItems(FXCollections.observableArrayList(
-                "NAM", "NỮ", "KHÁC"
+                "NAM", "NỮ", "KHÁC", "HỦY BỎ"
         ));
-        genderFilter.setValue("NAM");
+
 
         // Cấu hình filter ngày sinh từ
-        genderFilter1.setItems(FXCollections.observableArrayList(
-                "1980", "1990", "2000", "2010"
+        dobFromFilter.setItems(FXCollections.observableArrayList(
+                "1980", "1990", "2000", "2010", "HỦY BỎ"
         ));
-        genderFilter1.setValue("1980");
+
 
         // Cấu hình filter ngày sinh tới
-        genderFilter2.setItems(FXCollections.observableArrayList(
-                "1990", "2000", "2010", "2020"
+        dobToFilter.setItems(FXCollections.observableArrayList(
+                "1990", "2000", "2010", "2020", "HỦY BỎ"
         ));
-        genderFilter2.setValue("2020");
+
+        genderFilter.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+            if (newValue != null) {
+                handleGenderFilterInput(newValue);
+            }
+        });
+        dobFromFilter.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+            if (newValue != null) {
+                handleDobFromFilterInput(newValue);
+            }
+        });
+
+        dobToFilter.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+            if (newValue != null) {
+                handleDobToFilterInput(newValue);
+            }
+        });
 
     }
     @FXML
-    public void handleNewPatientButton(ActionEvent event) {
-        data.add(new PatientRecord(1, "John Doe", 101, LocalDate.of(1990, 1, 1),
-                PatientRecord.Gender.NAM, "123 Street", "0123456789", "john@email.com"));
+    public void handleApplyFilterButton(ActionEvent event) {
+        PatientRecord.Gender gender = null;
+        try{
+            gender = PatientRecord.Gender.valueOf(genderFilter.getValue().toString().toUpperCase());
+        }
+        catch(IllegalArgumentException ex){
+
+        }
+        dataView.clear();
+        System.out.println("LỌC ");
+        System.out.println(data.size());
+        for(int i = 0; i < data.size(); i++){
+            System.out.println(data.get(i).getGender().name());
+            if(gender == null || data.get(i).getGender() == gender){
+                dataView.add(data.get(i));
+            }
+        }
+        patientTable.setItems(dataView);
+
+    }
+    public void handleGenderFilterInput(String input) {
+        if(input.equals("HỦY BỎ")) {
+            genderFilter.getSelectionModel().clearSelection();
+            genderFilter.setValue("Giới Tính");
+        }
+        System.out.println(input);
+    }
+
+    public void handleDobFromFilterInput(String input) {
+        if(input.equals("HỦY BỎ")) {
+            dobFromFilter.getSelectionModel().clearSelection();
+            dobFromFilter.setValue("Sinh Từ");
+        }
+    }
+    public void handleDobToFilterInput(String input) {
+        if(input.equals("HỦY BỎ")){
+            dobToFilter.getSelectionModel().clearSelection();
+            dobToFilter.setValue("Sinh Tới");
+        }
 
     }
 }
