272fe824e1a6a27a1cbc3f81588f95e77a9b5aa7
MinhToanSBO <toanminh0404@gmail.com>
2025-10-11 23:27:59 +0700

    Update: Update router

--- OOP/mini-boot/src/main/java/org/miniboot/app/router/Router.java
+++ OOP/mini-boot/src/main/java/org/miniboot/app/router/Router.java
@@ -6,6 +6,7 @@ import org.miniboot.app.http.HttpResponse;
 import org.miniboot.app.http.HttpServer;
 
 import java.util.ArrayList;
+import java.util.Map;
 import java.util.function.Function;
 
 public class Router {
@@ -51,6 +52,22 @@ public class Router {
         post(path, handler, false);
     }
 
+    public void put(String path, Function<HttpRequest, HttpResponse> handler, boolean isProtected) {
+        routes.add(new Route(AppConfig.PUT_KEY, path, handler, isProtected));
+    }
+
+    public void put(String path, Function<HttpRequest, HttpResponse> handler) {
+        put(path, handler, false);
+    }
+
+    public void delete(String path, Function<HttpRequest, HttpResponse> handler, boolean isProtected) {
+        routes.add(new Route(AppConfig.DELETE_KEY, path, handler, isProtected));
+    }
+
+    public void delete(String path, Function<HttpRequest, HttpResponse> handler) {
+        delete(path, handler, false);
+    }
+
     public HttpResponse dispatch(HttpRequest request) throws Exception {
         Handler h = null;
         for (Route route : routes) {
@@ -61,6 +78,8 @@ public class Router {
                 }
                 else{
                     request.tags.put("protected", String.valueOf(route.isProtected));
+                    Map<String, String> params = route.path.extract(request.path);
+                    request.tags.putAll(params);
                     h = req -> route.handler.apply(req);
                 }
 
