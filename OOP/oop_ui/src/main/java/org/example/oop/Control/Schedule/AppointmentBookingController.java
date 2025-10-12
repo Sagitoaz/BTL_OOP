9b064b828e6268d5c21aedbe60eba93667d62faa
Nguyễn Thành Trung <122104636+Sagitoaz@users.noreply.github.com>
2025-10-12 14:42:53 +0700

    Add: Create AppointmentBookingController

--- OOP/oop_ui/src/main/java/org/example/oop/Control/Schedule/AppointmentBookingController.java
+++ OOP/oop_ui/src/main/java/org/example/oop/Control/Schedule/AppointmentBookingController.java
@@ -1,4 +1,27 @@
 package org.example.oop.Control.Schedule;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import org.example.oop.Model.Schedule.Appointment;
+import org.example.oop.Model.Schedule.AppointmentType;
+import org.example.oop.Model.Schedule.TimeSlot;
+
 public class AppointmentBookingController {
+    @FXML private TextField patientQuickSearch;
+    @FXML private Button btnNewPatient;
+    @FXML private ComboBox<String> cboCurrentUser;
+
+    @FXML private TextField txtPatientKeyword;
+    @FXML private TableView<Patient> tblPatients;
+    @FXML private Button btnSelectPatient;
+    @FXML private TextArea txtPatientName, txtPatientPhone, txtPatientEmail;
+
+    @FXML private ComboBox<Doctor> cboDoctors;
+    @FXML private DatePicker datePicker;
+    @FXML private ComboBox<TimeSlot> cboTimeSlot;
+    @FXML private ComboBox<AppointmentType> cboAppointmentType;
+    @FXML private TextArea txtNotes;
+    @FXML private Button btnSubmit;
+
+    @FXML private ListView<Appointment> lstUpcomingAppointments;
 }
