7a72df6d638e788935f11928043536ac27417fbd
MinhToanSBO <toanminh0404@gmail.com>
2025-10-13 12:34:28 +0700

    Update: Update module info

--- OOP/oop_ui/src/main/java/module-info.java
+++ OOP/oop_ui/src/main/java/module-info.java
@@ -20,4 +20,8 @@ module org.example.oop {
     exports org.example.oop;
     exports org.example.oop.Model;
     opens org.example.oop.Model to javafx.fxml;
+    exports org.example.oop.Data.models;
+    exports org.example.oop.Data.repositories;
+    exports org.example.oop.Data.storage;
+    opens org.example.oop.Data.models to javafx.fxml;
 }
