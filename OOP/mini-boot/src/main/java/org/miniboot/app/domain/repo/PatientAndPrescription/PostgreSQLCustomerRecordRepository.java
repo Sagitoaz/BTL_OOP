9bc4d4c57ba48ae34c9ab9a3e54498c34ae6e412
Dung1604005 <102669118+Dung1604005@users.noreply.github.com>
2025-10-17 23:38:03 +0700

    add: update and insert

--- OOP/mini-boot/src/main/java/org/miniboot/app/domain/repo/PatientAndPrescription/PostgreSQLCustomerRecordRepository.java
+++ OOP/mini-boot/src/main/java/org/miniboot/app/domain/repo/PatientAndPrescription/PostgreSQLCustomerRecordRepository.java
@@ -6,10 +6,9 @@ import org.miniboot.app.controllers.UserController;
 import org.miniboot.app.domain.models.Customer;
 import org.miniboot.app.util.CustomerConfig;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.Statement;
+import java.sql.*;
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 public class PostgreSQLCustomerRecordRepository implements  CustomerRecordRepository {
@@ -46,21 +45,65 @@ public class PostgreSQLCustomerRecordRepository implements  CustomerRecordReposi
             pstmt.setString(4, customer.getLastname());
             pstmt.setString(5, customer.getPhone());
             pstmt.setString(6, customer.getEmail());
-            pstmt.setObject(7, customer.getDob());
+            pstmt.setDate(7, Date.valueOf(customer.getDob()));
             pstmt.setString(8, customer.getGender().toString());
             pstmt.setString(9, customer.getAddress());
             pstmt.setString(10, customer.getNote());
-            pstmt.setObject(11, customer.getCreatedAt());
+            pstmt.setTimestamp(11, Timestamp.valueOf(customer.getCreatedAt()));
+            int affectedRows = pstmt.executeUpdate();
+            if (affectedRows == 0) {
+                throw new Exception("Inserting customer failed, no rows affected.");
+            }
+            try(ResultSet generatedKeys = pstmt.getGeneratedKeys()){
+                if(generatedKeys.next()){
+                    customer.setId(generatedKeys.getInt(1));
+                }
+                else{
+                    throw new Exception("Inserting customer failed, no ID obtained.");
+                }
+            }
         }
         catch (Exception e){
             System.err.println("❌ Error inserting appointment: " + e.getMessage());
             e.printStackTrace();
-            return null;
+            return Optional.empty();
         }
-        return null;
+        return Optional.of(customer);
     }
     private Optional<Customer> updateCustomer(Customer customer) {
-        return Optional.empty();
+        String sqlQuery = "UPDATE customers SET username=?, password=?, firstname=?, lastname=?, "+
+                "phone=?, email=?, dob=?, gender=?, address=?, note=?, created_at=? "+
+                "WHERE id=?;";
+        try (Connection conn = dbConfig.getConnection()) {
+            PreparedStatement pstmt = conn.prepareStatement(sqlQuery);
+            pstmt.setString(1, customer.getUsername());
+            pstmt.setString(2, customer.getPassword());
+            pstmt.setString(3, customer.getFirstname());
+            pstmt.setString(4, customer.getLastname());
+            pstmt.setString(5, customer.getPhone());
+            pstmt.setString(6, customer.getEmail());
+            pstmt.setDate(7, Date.valueOf(customer.getDob()));
+            pstmt.setString(8, customer.getGender().name());
+            pstmt.setString(9, customer.getAddress());
+            pstmt.setString(10, customer.getNote());
+            pstmt.setTimestamp(11, Timestamp.valueOf(customer.getCreatedAt()));
+            int affectedRows = pstmt.executeUpdate();
+            if (affectedRows == 0) {
+                throw new Exception("Updating customer failed, no rows affected.");
+            }
+        }
+        catch (Exception e){
+            System.err.println("❌ Error inserting appointment: " + e.getMessage());
+            e.printStackTrace();
+            return Optional.empty();
+        }
+        return Optional.of(customer);
+    }
+    public void saveAll(List<Customer> customers) {
+        String sqlQuery = "INSERT INTO customers (username, password, firstname, lastname, "+
+                "phone, email, dob, gender, address, note, created_at) "+
+                "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);";
+
     }
 
 
