5e8afee38d414557def7ec5ffc0e25e61aba7919
PHANMINHHIEU05 <phanhieu972005@gmail.com>
2025-10-18 16:17:05 +0700

    Fix

--- OOP/oop_ui/src/main/java/org/example/oop/Model/Inventory/Enum/MovementType.java
+++
@@ -1,8 +0,0 @@
-package org.example.oop.Model.Inventory.Enum;
-
-/**
- * Enum định nghĩa các loại giao dịch stock
- */
-public enum MovementType {
-    PURCHASE, SALE, RETURN_IN, RETURN_OUT, ADJUSTMENT, CONSUME, TRANSFER, OPENING
-}
