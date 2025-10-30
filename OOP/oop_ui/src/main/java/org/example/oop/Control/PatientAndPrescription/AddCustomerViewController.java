390822cac224b19643a550810f1363755b31ec37
Dung1604005 <102669118+Dung1604005@users.noreply.github.com>
2025-10-30 21:30:37 +0700

    fix: change css

--- OOP/oop_ui/src/main/java/org/example/oop/Control/PatientAndPrescription/AddCustomerViewController.java
+++ OOP/oop_ui/src/main/java/org/example/oop/Control/PatientAndPrescription/AddCustomerViewController.java
@@ -11,6 +11,7 @@ import javafx.stage.Stage;
 import org.example.oop.Service.CustomerRecordService;
 import org.example.oop.Utils.SceneManager;
 import org.miniboot.app.domain.models.CustomerAndPrescription.Customer;
+import org.miniboot.app.domain.models.UserRole;
 
 import java.net.URL;
 import java.time.LocalDate;
@@ -38,10 +39,16 @@ public class AddCustomerViewController implements Initializable
     public void initialize(URL url, ResourceBundle rb) {
         genderComboBox.getItems().addAll(Customer.Gender.values());
         curCustomer = null;
+        UserRole role = (UserRole) SceneManager.getSceneData("role");
+        if( role.name().equalsIgnoreCase("CUSTOMER")){
+            curCustomer = (Customer) SceneManager.getSceneData("accountData");
+        }
+        if(curCustomer != null){
+            initData(curCustomer);
 
+        }
 
     }
-
 
 
     public void initData(Customer customer){
