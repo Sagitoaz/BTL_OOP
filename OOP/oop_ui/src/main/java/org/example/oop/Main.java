de3e09c1064aa2c6725331a7dbb6956c5d1d2866
Dung1604005 <102669118+Dung1604005@users.noreply.github.com>
2025-11-04 18:08:03 +0700

    fix: error cant load calender

--- OOP/oop_ui/src/main/java/org/example/oop/Main.java
+++ OOP/oop_ui/src/main/java/org/example/oop/Main.java
@@ -17,6 +17,6 @@ public class Main extends Application {
         SceneManager.setPrimaryStage(stage);
         SceneManager.clearCache();
         SceneManager.clearSceneData();
-        SceneManager.switchScene(SceneConfig.ADMIN_DASHBOARD_FXML, SceneConfig.Titles.LOGIN);
+        SceneManager.switchScene(SceneConfig.LOGIN_FXML, SceneConfig.Titles.LOGIN);
     }
 }
