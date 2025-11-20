598a2d74d9593d8e7806aafe3cad18fba8e8b352
PHANMINHHIEU05 <phanhieu972005@gmail.com>
2025-11-20 15:17:31 +0700

    Refactor Main class imports for clarity, update ApiConfig to use DEVELOPMENT environment, and adjust AddCustomerView layout by removing redundant navigation buttons.

--- OOP/oop_ui/src/main/java/org/example/oop/Main.java
+++ OOP/oop_ui/src/main/java/org/example/oop/Main.java
@@ -1,11 +1,12 @@
 package org.example.oop;
 
-import javafx.application.Application;
-import javafx.stage.Stage;
+import java.io.IOException;
+
 import org.example.oop.Utils.SceneConfig;
 import org.example.oop.Utils.SceneManager;
 
-import java.io.IOException;
+import javafx.application.Application;
+import javafx.stage.Stage;
 
 public class Main extends Application {
     public static void main(String[] args) {
@@ -16,7 +17,7 @@ public class Main extends Application {
     public void start(Stage stage) throws IOException {
         SceneManager.setPrimaryStage(stage);
         SceneManager.clearCache();
-        SceneManager.clearSceneData();  
+        SceneManager.clearSceneData();
         SceneManager.switchScene(SceneConfig.LOGIN_FXML, SceneConfig.Titles.LOGIN);
     }
 }
